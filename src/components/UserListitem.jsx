c6ac3d2b6b56c1677f3edb51387fe430c941bef8
Valentin Hristov <134092161+valio90@users.noreply.github.com>
2026-09-24 13:52:42 +0300

    Fixed date

--- src/components/UserListitem.jsx
+++ src/components/UserListitem.jsx
@@ -1,3 +1,5 @@
+import { fromIsoDate } from "../utils/dateTimeUtils";
+
 export default function UserListItem(
     {
         firstName,
@@ -25,7 +27,7 @@ export default function UserListItem(
             <td>{lastName}</td>
             <td>{email}</td>
             <td>{phoneNumber}</td>
-            <td>{createdAt}</td>
+            <td>{fromIsoDate(createdAt)}</td>
 
             <td className="actions">
                 <button className="btn edit-btn" title="Edit">
