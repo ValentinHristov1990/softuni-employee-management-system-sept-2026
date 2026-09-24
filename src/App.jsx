e064242e1d18da6a687da561e81e032c3238976a
Valentin Hristov <134092161+valio90@users.noreply.github.com>
2026-09-24 13:42:22 +0300

    Show dynamic users

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import CreateEdit from './components/CreateEditSection';
 import Footer from './components/FooterSection';
 import Header from './components/HeaderSection';
@@ -8,8 +10,21 @@ import UserDetails from './components/UserDetails';
 import UserList from './components/UserList';
 import UserSearch from './components/UserSearch';
 import './styles.css';
+
 function App() {
 
+  const [users, setUsers] = useState([]);
+  console.log(users)
+  useEffect(() => {
+    fetch('https://ggoxncimrywupljnjvuz.supabase.co/rest/v1/users', {
+      headers: {
+        'apikey': 'sb_publishable_H3KNXiPjTSLequcG7Isvkw_zWGrOK7D'
+      }
+    })
+      .then(res => res.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error("Error fetching users:", error));
+  }, []);
   return (
     <>
       <Header />
@@ -19,7 +34,8 @@ function App() {
         <section className="card users-container">
           {/* <Spinner /> */}
           <UserSearch />
-          <UserList />
+          <UserList users={users} />
+          <button className="btn-add btn">Add new user</button>
           <Pagination />
         </section>
 
